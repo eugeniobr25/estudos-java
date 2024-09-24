20ecb88f84a3eb0c5c4b31f923fc3c6f0daa3191
eugeniobr25 <eugeniobrilhante@hotmail.com>
2024-09-23 21:32:21 -0300

    finalizando exercicios da aula 13

--- hello-word-vscode-java/src/ExerAula13/Exer17.java
+++ hello-word-vscode-java/src/ExerAula13/Exer17.java
@@ -13,15 +13,37 @@ public class Exer17 {
 
         System.out.println("Você quer pintar quantos metros quadrados?");
         double area = scan.nextDouble();
+
+        // Acrescenta 10% de folga na área a ser pintada
+        area = area * 1.10;
+
         double quantTinta = area / 3;
+
+        // 1. Situação: Apenas latas de 18 litros
         double quantBaldeTinta = Math.ceil(quantTinta / baldeTinta);
-        double quantGalaoTinta = Math.ceil(quantTinta / galaoTinta);
         double valorTotalBalde = quantBaldeTinta * valorBaldeTinta;
+
+        // 2. Situação: Apenas galões de 3,6 litros
+        double quantGalaoTinta = Math.ceil(quantTinta / galaoTinta);        
         double valorTotalGalao = quantGalaoTinta * valorGalaoTinta;
 
+        // 3. Situação: Misturar latas e galões para minimizar o custo
+        // Calcula quantas latas de 18L são necessárias
+        int latasNecessarias = (int) (quantTinta / baldeTinta);
+        double restoTinta = quantTinta % baldeTinta; // Quanto de tinta ainda falta
+
+        // Calcula quantos galões serão necessários para o resto
+        double galaoParaRestante = Math.ceil(restoTinta / galaoTinta);
+
+        // Custo da combinação de latas e galões
+        double valorTotalMisto = (latasNecessarias * valorBaldeTinta) + (galaoParaRestante * valorGalaoTinta);
+
+
         
-        System.out.println("A quantidade em baldes é " + quantBaldeTinta + " e o valor é R$ " + valorTotalBalde);
-        System.out.println("A quantidade em galões é " + quantGalaoTinta + " e o valor é R$ " + valorTotalGalao);
+        System.out.println("A quantidade em baldes é " + quantBaldeTinta + " | Valor: R$ " + valorTotalBalde);
+        System.out.println("A quantidade em galões é " + quantGalaoTinta + " | Valor: R$ " + valorTotalGalao);
+        System.out.println("Quantidade de latas: " + latasNecessarias + " | Quantidade de galões: " + galaoParaRestante);
+        System.out.println("Valor total da combinação: R$ " + valorTotalMisto);
         
     }
 }
