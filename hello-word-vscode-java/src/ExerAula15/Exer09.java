8e6a7d917f7e04e3c06f020e8960d6123e0c8ed0
eugeniobr25 <eugeniobrilhante@hotmail.com>
2024-09-25 13:14:41 -0300

    subindo exercicios

--- hello-word-vscode-java/src/ExerAula15/Exer09.java
+++ hello-word-vscode-java/src/ExerAula15/Exer09.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Exer09 {
 
     public static void main(String[] args) {
-        
+
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Entre com o primero número:");
@@ -18,41 +18,23 @@ public class Exer09 {
         int num3 = scan.nextInt();
 
         if (num1 <= num2 && num1 <= num3 && num2 <= num3) {
-            // 1
-            // 2
-            // 3
-            // num3 < num2 < num1
-            System.out.println(num3 " - " + num2 + " - " + num1);
-        } else if (num2 <= num1 && num1 <= num3 && num2 <= num3) {
-            // 2
-            // 1
-            // 3
-            // num3 < num1 < num2
-            System.out.println(num3 " - " + num1 + " - " + num2);
-        } else if (num3 <= num1 && num1 <= num2 && num3 <= num2) {
-            // 2
-            // 3
-            // 1
-            // num2 < num1 < num3
-            System.out.println(num2 " - " + num1 + " - " + num3);
-        }else if (num1 <= num3 && num3 <= num2 && num1 <= num2) {
-            // 1
-            // 3
-            // 2
-            // num2 < num3 < num1
-            System.out.println(num2 " - " + num3 + " - " + num1);
-        }else if (num3 <= num2 && num2 <= num1 && num3 <= num1) {
-            // 3
-            // 2
-            // 1
             // num1 < num2 < num3
-            System.out.println(num1 " - " + num2 + " - " + num3);
-        } else if (num2 <= num3 && num3 <= num1 && num2 <= num1) {
-            // 3
-            // 1
-            // 2
+            System.out.println(num1 + " - " + num2 + " - " + num3);
+        } else if (num2 <= num1 && num2 <= num3 && num1 <= num3) {
+            // num2 < num1 < num3
+            System.out.println(num2 + " - " + num1 + " - " + num3);
+        } else if (num3 <= num1 && num3 <= num2 && num1 <= num2) {
+            // num3 < num1 < num2
+            System.out.println(num3 + " - " + num1 + " - " + num2);
+        } else if (num1 <= num3 && num1 <= num2 && num3 <= num2) {
             // num1 < num3 < num2
-            System.out.println(num1 " - " + num3 + " - " + num2);
+            System.out.println(num1 + " - " + num3 + " - " + num2);
+        } else if (num3 <= num2 && num3 <= num1 && num2 <= num1) {
+            // num3 < num2 < num1
+            System.out.println(num3 + " - " + num2 + " - " + num1);
+        } else if (num2 <= num3 && num2 <= num1 && num3 <= num1) {
+            // num2 < num3 < num1
+            System.out.println(num2 + " - " + num3 + " - " + num1);
         }
     }
 
